7f3f9adb7fa85baa9ed04aa1ce22f2cd43442922
Lucas Pediconi <lucaspediconi@hotmail.com>
2025-07-30 16:42:32 -0300

    Criação de arquivo JS

---
+++ js/02-meu-script.js
@@ -0,0 +1,27 @@
+/* Exercícios */
+
+// 1) Crie uma variável chamada "uc" e coloque nela o número da UC que estamos
+let uc = 2;
+console.log(`Estamos na Uc ${uc}`);
+
+// 2) Crie uma variável chamada "competencia" e coloque nela uma frase que represente o que estamos estudando nesta UC.
+let competencia = "Front-End com Javascript";
+console.log(competencia);
+
+// 3) Monte e mostre uma frase no console exibindo a UC e a competência. Exemplo: Estamos na UC2 estudando Front-End com JavaScript.
+console.log(`Olá, estamos na Uc ${uc} e estamos aprendendo ${competencia}.`)
+
+//ERRADO: console.log("Gosto "muito" de paçoca")
+
+//CORRETO
+console.log('Gosto "muito" de paçoca')
+
+//CORRETO
+console.log("Gosto 'muito' de paçoca")
+
+// CORRETO
+console.log("Gosto \"muito\" de paçoca")
+
+
+
+
