4c24f86617a82514421367e9218bb6464ea5c52b
Lucas Pediconi <lucaspediconi@hotmail.com>
2025-08-06 16:05:09 -0300

    Array como Matriz de 2 dimensões

--- js/03-arrays.js
+++ js/03-arrays.js
@@ -20,5 +20,25 @@ console.log(coisas[1]);
 console.log(coisas[4]);
 
 
+/* Exercícios (FAÇA AQUI MESMO!) */
+
+// 1) Crie um array contendo o nome de 7 coisas que vc gosta (exemplos: artistas, músicas, livros, comida... sei lá, inventa!)
+
+let animes = ["Naruto", "Jujutsu", "Black Clover", "Blue Lock", "Fire Force", "Kaiju no 8", "Solo Leveling"];
+
+// 2) Em seguida, mostre no console uma frase personalizada indicando o segundo, o quinto e o sétimo elemento do array. Use concatenação e/ou template string.
+console.log(`Gosto bastante de animes como: ${animes[1]}, ${animes[4]} e ${animes [6]}`)
+
+//Array como Matriz de 2 dimensões
+const tecnologias = [
+    ["HTML", "CSS", "JS"],
+    ["Figma", "Photoshop"],
+    ["PHP", "Node.js", "SQL", "Express"]
+]
+
+//console.log(tecnologias [0])
+console.log(tecnologias[0][2]);
+console.log(tecnologias[1][0]);
+console.log(tecnologias[2][3]);
 
 
