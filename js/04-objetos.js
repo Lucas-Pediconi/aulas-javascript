c8efcd262eccbaf38512d465f726c12b7c6fa85d
Lucas Pediconi <lucaspediconi@hotmail.com>
2025-08-07 13:33:34 -0300

    Exercicios de JS

--- js/04-objetos.js
+++ js/04-objetos.js
@@ -50,3 +50,41 @@ const livros =  [
 console.log(livros);
 
 console.log(livros[2].titulo);
+
+
+//Ecercicios
+
+/* Exercícios
+
+1. Crie um objeto chamado "aluno" contendo os seguintes dados:
+
+- Nome Completo
+- Data de Nascimento
+- (Use ARRAY) Lista de telefones (fixo e celular)
+- (DESAFIO: use um OBJETO nesta propriedade) Endereço contendo (separadamente):
+  - rua
+  - número
+  - bairro
+
+2. Mostre no console o nome do aluno, o telefone celular e o bairro em que mora. */
+
+
+const aluno = {
+    nome_completo: "Lucas Pediconi",
+    data_nascimento: "13/02/2005",
+    lista_telefones: [
+        "11 98752-3536",
+        "11 94214-0909"
+    ],
+    endereço: {
+        rua:"R Epaminondas",
+        numero:122,
+        bairro:"JD Roberto",
+    }
+}
+
+console.log(`Eu me chamo ${aluno.nome_completo} nasci no dia ${aluno.data_nascimento}, meu telefone é ${aluno.lista_telefones[1]} e moro na ${aluno.endereço.rua} número ${aluno.endereço.numero} bairro ${aluno.endereço.bairro}`)
+
+
+
+
