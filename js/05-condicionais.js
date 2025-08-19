cf36ef68cdf448effc449b713b6bdfed0b52b108
Lucas Pediconi <lucaspediconi@hotmail.com>
2025-08-19 14:56:23 -0300

    Condicional COMPOSTA (if/else)

--- js/05-condicionais.js
+++ js/05-condicionais.js
@@ -10,6 +10,20 @@ console.log("Exemplo 1");
 let numero = 50;
 
 // Condicional SIMPLES (usa apenas if)
-if(numero >= 10){
+if (numero >= 10) {
     console.log("Se você está vendo essa mensagem é porque a condição é VERDADEIRA!");
+}
+
+console.log("\nExemplo 2")
+
+let aluno = "Lucas";
+let idade = 18;
+
+//Condicional COMPOSTA (if/else)
+
+if (idade < 18) {
+    console.log("O aluno é menor de idade");
+}
+else {
+    console.log("O aluno é maior de idade");
 }
